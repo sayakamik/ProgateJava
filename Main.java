e992c495f555d09101269f291c58bb0ead6629dd
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-11-28 13:34:15 +0000

    変数

--- Main.java
+++ Main.java
@@ -1,6 +1,52 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("こんにちは、Java");
-        System.out.println("5 + 3");
+        int number1 = 3;
+
+        // int型の変数number2を定義し、7を代入してください
+        int number2 = 7;
+
+        // number1 * number2を出力してください
+        System.out.println(number1 * number2);
+
+        // 変数textに「プログラミングを勉強しよう」を代入してください
+        String text = "プログラミングを勉強しよう";
+
+        // 「Progateで」と変数textを連結して出力してください
+        System.out.println("Progateで" + text);
+
+        // 変数number1を上書きする
+        number1 = 5;
+        System.out.println(number1 * number2);
+
+        // 変数textを上書きする
+        text = "Javaを勉強しよう";
+        System.out.println("Progateで" + text);
+
+        // 自己代入する
+        number1 = number1 + 1;
+        System.out.println(number1);
+
+        // 自己代入の省略 number1+1, number2*7
+        number1 ++;
+        System.out.println(number1);
+        number2 *= 2;
+        System.out.println(number2);
+
+        // 三角形、四角形の面積を出す。
+        int length = 3;
+        int height = 4;
+        int rectangleArea = length *height;
+        int triangleArea = length *height/2;
+        System.out.println(rectangleArea);
+        System.out.println(triangleArea);
+
+        // 少数の変数 double型
+        double number3 = 8.5;
+        System.out.println(number3);
+
+        double number4 = 3.4;
+        System.out.println(number3 + number4);
+        
+        
     }
 }
